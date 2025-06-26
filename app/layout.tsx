129e5cada405be76b691a0a4c62cf75fd10a51a0
ghazy2001 <mahmoudghazy2001@gmail.com>
2025-06-26 17:02:51 +0300

    Upload project files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import "@/app/ui/global.css";
+import { inter, lousitana } from "@/app/ui/fonts";
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${(inter.className, lousitana.className)} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
